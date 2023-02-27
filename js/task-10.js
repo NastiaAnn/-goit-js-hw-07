477956230640e58e0726e65f3d8ed3ba2e2517e4
NastiaAnn <anastasianazar4@gmail.com>
2023-02-27 12:58:16 +0200

    27.02 12:58

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBoxBtn = document.querySelector('button[data-create]');
+const destroyBoxBtn = document.querySelector('button[data-destroy]');
+const mainContainer = document.querySelector('#boxes');
+
+const inputEl = document.querySelector('[type="number"]');
+
+// createBoxBtn.addEventListener('click', handleNewBoxCreation);
+
+// function handleNewBoxCreation(event) {
+//   const result = handleNumberValue();
+//   const coloredContainer = document.createElement('div');
+//   coloredContainer.classList.add('colored_container');
+//   mainContainer.append(coloredContainer);
+
+// }
